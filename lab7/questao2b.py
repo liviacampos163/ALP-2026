38a45cfe993a80d769cf40ce938f15e709cf50fe
liviacampos163 <liviacamposdasilva015@gmail.com>
2026-06-25 21:07:37 -0300

    lab7

--- lab7/questao2b.py
+++
@@ -1,7 +0,0 @@
-soma = 0
-while soma <= 10: 
-    num = int(input("Digite um número para somar: "))
-    soma += num
-
-#print na tela com o resultado da soma
-print(f"Resultado da soma: {soma}")
--- lab7/questao2c.py
+++
@@ -1,9 +0,0 @@
-maior = float('-inf')
-soma = 0
-while soma <= 10: 
-    num = int(input("Digite um número: "))
-    if num > maior:
-       maior = num
-print('O maior número é', maior)
-
-#  soma sem variavel
